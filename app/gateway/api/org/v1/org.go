17473c5aca0f500bfaac89dcbfc00a806d671e85
小无 <zhystc0012@163.com>
2025-11-03 08:59:29 +0800

    1

--- app/gateway/api/org/v1/org.go
+++ app/gateway/api/org/v1/org.go
@@ -19,9 +19,9 @@ type GetListRes struct {
 }
 
 type GetListItem struct {
-	Id       int64  `json:"id" dc:"主键"`              // 主键
-	Name     string `json:"name" dc:"组织名称"`        // 组织名称
-	Code     string `json:"code" dc:"组织编码"`        // 组织编码
+	Id       int64  `json:"id" dc:"主键"`          // 主键
+	Name     string `json:"name" dc:"组织名称"`      // 组织名称
+	Code     string `json:"code" dc:"组织编码"`      // 组织编码
 	Type     int64  `json:"type" dc:"组织分类，枚举"`   // 组织分类，枚举
 	Status   int32  `json:"status" dc:"组织状态，枚举"` // 组织状态，枚举
 	CreateAt string `json:"createAt" dc:"创建时间"`
@@ -33,10 +33,10 @@ type GetReq struct {
 }
 
 type GetRes struct {
-	Id          int64         `json:"id" dc:"主键"`              // 主键
-	ParentId    int64         `json:"parentId" dc:"父id"`        // 父id
-	Name        string        `json:"name" dc:"组织名称"`        // 组织名称
-	Code        string        `json:"code" dc:"组织编码"`        // 组织编码
+	Id          int64         `json:"id" dc:"主键"`          // 主键
+	ParentId    int64         `json:"parentId" dc:"父id"`   // 父id
+	Name        string        `json:"name" dc:"组织名称"`      // 组织名称
+	Code        string        `json:"code" dc:"组织编码"`      // 组织编码
 	Type        int64         `json:"type" dc:"组织分类，枚举"`   // 组织分类，枚举
 	Status      int64         `json:"status" dc:"组织状态，枚举"` // 组织状态，枚举
 	Supervisors []*Supervisor `json:"supervisors" dc:"部门主管列表"`
